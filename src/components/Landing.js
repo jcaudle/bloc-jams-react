a06e780751feafafdb8ef4182ddb9315240e3684
Joseph Caudle <joseph@josephcaudle.com>
2017-11-03 00:40:15 -0400

    Mark up the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const Landing = () => (
-  <section className="landing">
-    Landing page will go here
-  </section>
+  <div>
+    <h1 className="hero-title">Turn the music up!</h1>
+
+    <section className="selling-points">
+      <div className="point">
+        <h2 className="point-title">Choose your music</h2>
+        <p classname="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Unlimited, etc</h2>
+        <p className="point-description">You don't like limits!</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Mobile enabled</h2>
+        <p className="point-description">Yay! Devices!</p>
+      </div>
+    </section>
+  </div>
 )
 
 export default Landing;
