75c312d5f1d20ae1d755a1eebc66847686ea2eef
Joseph Caudle <joseph@josephcaudle.com>
2017-11-03 01:01:40 -0400

    Set up Library and Album components

---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.match.params.slug} Album will go here
+      </div>
+    );
+  }
+}
+
+export default Album;
